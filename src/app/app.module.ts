5b90d260a4931aa08077c06497a077660465c798
fasrinaleem <fasrinaleem@gmail.com>
2020-09-08 15:47:30 +0530

    Icons Added, Minor UI Changes Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from '././modules/core/navbar/navbar.component';
+import { SidebarComponent } from '././modules/core/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CoreLayoutComponent } from './modules/core-layout/core-layout.component';
-import { WidgetBarComponent } from './modules/core/widget-bar/widget-bar/widget-bar.component';
+import { WidgetBarComponent } from './modules/core/widget-bar/widget-bar.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,8 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
-    NgxChartsModule
+    NgxChartsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/core/widget-bar/widget-bar.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { singlePieChart, singleBarChart, multiBarChart, multiBarChart2 } from "../../core-layout/chart-data";
+
+@Component({
+  selector: 'app-widget-bar',
+  templateUrl: './widget-bar.component.html',
+  styleUrls: ['./widget-bar.component.css']
+})
+export class WidgetBarComponent implements OnInit {
+
+
+  //Piechart
+  singlePieChart: any[];
+
+  viewPieChart: any[] = [400, 200];
+
+  // options
+  showLegendPieChart = false;
+
+  colorSchemePieChart = {
+    domain: ['#1f71ad', '#fc9003', '#b5b3ae']
+  };
+
+  // pie
+  showLabelsPieChart = false;
+  explodeSlicesPieChart = false;
+  doughnutPieChart = false;
+
+
+  //Bar Chart
+  singleBarChart: any[];
+
+  viewBarChart: any[] = [150, 100];
+
+  // options
+  showXAxisBarChart = false;
+  showYAxisBarChart = true;
+  gradient = false;
+  showLegendBarChart = false;
+  showXAxisLabelBarChart = false;
+  showYAxisLabelBarChart = false;
+  showXAxisLabelMultiBarChart = false;
+  showYAxisLabelMultiBarChart = false;
+
+  colorSchemeBarChart = {
+    domain: ['#1f71ad', '#1f71ad', '#1f71ad', '#1f71ad']
+  };
+
+  //Multi Bar Chart
+  multiBarChart: any[];
+  multiBarChart2: any[];
+
+  viewMultiBarChart: any[] = [250, 200];
+
+  // options
+  showXAxisMultiBarChart = true;
+  showYAxisMultiBarChart = true;
+  gradientMultiBarChart = false;
+  showLegendMultiBarChart = false;
+  xAxisLabelBarChart = false;
+  yAxisLabelBarChart = false;
+
+  colorSchemeMultiBarChart = {
+    domain: ['#1f71ad', '#fc9003']
+  };
+
+  constructor() {
+    Object.assign(this, { singlePieChart, singleBarChart, multiBarChart, multiBarChart2 })
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+  ngOnInit(): void {
+  }
+  openNav() {
+    document.getElementById("myWidgetBar").style.width = "250px";
+  }
+
+  closeNav() {
+    document.getElementById("myWidgetBar").style.width = "0";
+  }
+
+}
